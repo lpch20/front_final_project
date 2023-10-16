1352fb993752c6882b9517afc7cc7b7f0dae67a0
Ignacio Lopez <nacholopez18@hotmail.com>
2023-10-16 19:26:16 -0300

    front home y search

---
+++ src/components/Navegationbar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { Outlet, Link, NavLink } from "react-router-dom";
+import "./Navegationbar.css";
+
+function Navegationbar(props) {
+  return (
+    <>
+      <div className="navegationContainer">
+        <Link to="/">
+          <button className="homeBtn">
+            <img src={props.icon1} />
+            Inicio
+          </button>
+        </Link>
+        <Link to="/top20">
+          <button className="searchBtn">
+            <img src={props.icon2} />
+            Buscador
+          </button>
+        </Link>
+        <button className="profileBtn">
+          <img src={props.icon3} />
+          Perfil
+        </button>
+        <button className="friendsBtn">
+          <img src={props.icon4} />
+          Amigos
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Navegationbar;
---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { Outlet, Link, NavLink } from "react-router-dom";
+
+function Searchbar() {
+  return <></>;
+}
+
+export default Searchbar;
