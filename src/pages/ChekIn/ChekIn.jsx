b7e7899e203d31800619b2f3834554bffca0948c
Ignacio Lopez <nacholopez18@hotmail.com>
2023-10-16 20:32:20 -0300

    arreglos de merge

--- src/pages/ChekIn/ChekIn.jsx
+++ src/pages/ChekIn/ChekIn.jsx
@@ -1,32 +1,26 @@
-
 import React from "react";
 
-import './ChekIn.css';
-import MainButton from '../../components/MainBtn'
+import "./ChekIn.css";
+import MainButton from "../../components/MainBtn";
 export const ChekIn = () => {
   return (
-    <> 
-    <main className="container" >
-      <div className="logo">
-        <h1 className="lettersLogo">audn
-          </h1>
+    <>
+      <main className="container">
+        <div className="logo">
+          <h1 className="lettersLogo">audn</h1>
           <br />
-          
-      </div>
-      <div  className="mid">
-      <h2 className="lettersMid">Música a medida.</h2></div>
-      <br />
-      <div  className="containter2" >
-        <MainButton>Registrarse gratis</MainButton>
-        <MainButton>Continuar Con Google</MainButton>
-        <MainButton>Continuar Con Apple</MainButton>
-       
-
-      </div>
-      <button className="logIn">
-
-      </button>
-    </main>
+        </div>
+        <div className="mid">
+          <h2 className="lettersMid">Música a medida.</h2>
+        </div>
+        <br />
+        <div className="containter2">
+          <MainButton>Registrarse gratis</MainButton>
+          <MainButton>Continuar Con Google</MainButton>
+          <MainButton>Continuar Con Apple</MainButton>
+        </div>
+        <button className="logIn"></button>
+      </main>
     </>
   );
 };
